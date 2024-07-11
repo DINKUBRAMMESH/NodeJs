2b86f32ee2a91ff1daf2b80e1e4647c188bf330a
DINKUBRAMMESH <dinkubrammesh07@gmail.com>
2024-07-11 13:26:46 +0530

    learning bcrypt

--- Bcrypt module/bcrypt.js
+++ Bcrypt module/bcrypt.js
@@ -1,20 +1,52 @@
-import bcrypt from 'bcrypt'
+// import bcrypt from 'bcrypt'
+
+// let userDetails = {
+//     uid: "1",
+//     upassword: "1234",
+//     uemail: "uemail@gmail.com"
+// }
+
+// let hasedPassword = bcrypt.hashSync(userDetails.upassword,10)
+
+// userDetails = {
+//     ...userDetails,upassword:hasedPassword
+// }
+
+// if (bcrypt.compareSync("1234",userDetails.upassword)){
+//     console.log("correct password")
+// }
+// else{
+//     console.log("incorrect password");
+// }
+
+
+
+
+
+//async
+import bcrypt from 'bcrypt';
 
 let userDetails = {
     uid: "1",
     upassword: "1234",
     uemail: "uemail@gmail.com"
-}
+};
 
-let hasedPassword = bcrypt.hashSync(userDetails.upassword,10)
+bcrypt.hash(userDetails.upassword, 10, (err, hashedPassword) => {
+    if (err) throw err;
 
-userDetails = {
-    ...userDetails,upassword:hasedPassword
-}
+    userDetails = {
+        ...userDetails,
+        upassword: hashedPassword
+    };
 
-if (bcrypt.compareSync("1234",userDetails.upassword)){
-    console.log("correct password")
-}
-else{
-    console.log("incorrect password");
-}
+    bcrypt.compare("1234", userDetails.upassword, (err, correct) => {
+        if (err) throw err;
+
+        if (correct) {
+            console.log("Correct password");
+        } else {
+            console.log("Incorrect password");
+        }
+    });
+});
