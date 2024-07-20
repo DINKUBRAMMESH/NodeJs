615c31424fe2a6411978d78eea8c0a530e3c0f69
DINKUBRAMMESH <dinkubrammesh07@gmail.com>
2024-07-20 09:28:13 +0530

    es6

---
+++ Sillyname Generator/sillyname.js
@@ -0,0 +1,11 @@
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+
+// console.log(sillyName)
+
+
+import sillyname from 'sillyname';
+
+const sillyName = sillyname();
+console.log(sillyName);
+
